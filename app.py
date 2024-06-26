934ad2f215d2a4e359c2ddb9ecb291db3463daf3
Vadlagattu <vinay.vgu@gmail.com>
2024-06-26 21:28:10 +1000

    Your commit message

---
+++ app.py
@@ -0,0 +1,19 @@
+from openai import OpenAI
+
+client = OpenAI(
+  base_url = "https://integrate.api.nvidia.com/v1",
+  api_key = "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"
+)
+
+completion = client.chat.completions.create(
+  model="meta/llama3-70b-instruct",
+  messages=[{"role":"user","content":"Write a limerick about the wonders of GPU computing."}],
+  temperature=0.5,
+  top_p=1,
+  max_tokens=1024,
+  stream=True
+)
+
+for chunk in completion:
+  if chunk.choices[0].delta.content is not None:
+    print(chunk.choices[0].delta.content, end="")
